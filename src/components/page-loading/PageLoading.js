11af1f66453a8a8dfb9d2599cd8cc81800e532ae
Gerald <gejocadev@gmail.com>
2021-09-07 11:14:48 -0500

    v1.0

---
+++ src/components/page-loading/PageLoading.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './PageLoading.module.scss';
+
+const PageLoading = () => {
+    return (
+        <div className={classes.loading}>
+            <span>Z</span>
+            <span>A</span>
+            <span>F</span>
+            <span>I</span>
+            <span>R</span>
+            <span>O</span>
+            <span>A</span>
+            <span>P</span>
+            <span>P</span>
+        </div>
+    );
+};
+
+export default PageLoading;
