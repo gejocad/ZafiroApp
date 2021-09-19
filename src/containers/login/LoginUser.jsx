6bd6de3cc0dd3a121f231b8c838eede030a31542
Wildelmy <wildelmycolina@gmail.com>
2021-09-19 00:54:43 -0500

    Agrega login usuario

---
+++ src/containers/login/LoginUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LoginUser = () => {
+    return (
+        <div>
+            <h3>
+                Para ver tus certificados ingresa tu tipo y numero de documento 
+
+            </h3>
+        </div>
+    )
+}
+
+export default LoginUser
