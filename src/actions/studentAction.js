7d3782a83f828af47799e3d440616421c4e006a7
Wildelmy <wildelmycolina@gmail.com>
2021-09-21 04:27:23 -0500

    crear link para general certificados

--- src/actions/studentAction.js
+++ src/actions/studentAction.js
@@ -7,26 +7,26 @@ import Swal from "sweetalert2";
 let fileUrl = [];
 
 export const AddStudent = (student, typedoc, prog) => {
-    return async (dispatch) => {
-        const {name, lastName, document, email, finscrip} = student
+  return async (dispatch) => {
+    const { name, lastName, document, email, finscrip } = student
 
-        const newStudent = {
-            name,
-            lastName,
-            fullName: name + ' ' + lastName,
-            typedoc: typedoc,
-            document,
-            email,
-            finscrip,
-            prog: prog,
-        }
-
-        console.log(newStudent);
-
-        await db.collection('students/').add(newStudent)
-        dispatch(addNewStudent(newStudent))
-        startLoadingStudent()
+    const newStudent = {
+      name,
+      lastName,
+      fullName: name + ' ' + lastName,
+      typedoc: typedoc,
+      document,
+      email,
+      finscrip,
+      prog: prog,
     }
+
+    console.log(newStudent);
+
+    await db.collection('students').doc(document).set(newStudent)
+    dispatch(addNewStudent(newStudent))
+    startLoadingStudent()
+  }
 }
 export const addNewStudent = (student) => ({
   type: types.addStudent,
@@ -36,10 +36,10 @@ export const addNewStudent = (student) => ({
 });
 
 export const startLoadingStudent = () => {
-    return async (dispatch) => {
-        const student = await loadStudents()
-        dispatch(setStudent(student))
-    }
+  return async (dispatch) => {
+    const student = await loadStudents()
+    dispatch(setStudent(student))
+  }
 }
 
 export const setStudent = (student) => ({
@@ -55,87 +55,87 @@ export const activeStudents = (id, student) => ({
   },
 });
 
-export const Edit = (student, typedoc, prog) => {
-    return async (dispatch) => {
-        
-        const EditStudent = {
-            name: student.name,
-            lastName: student.lastName,
-            fullName: student.name + ' ' + student.lastName,
-            typedoc: typedoc,
-            document: student.document,
-            email: student.email,
-            finscrip: student.finscrip,
-            prog: prog,
-        }
-
-        const studentF = { ...EditStudent  }
-        delete studentF.id
-
-        Swal.fire({
-            title: 'actualizando...',
-            text: 'Por favor, Espere ...',
-allowOutsideClick: false,
-            onBeforeOpen: () => {
-                Swal.showLoading()
-            }
-        })
-
-        await db.doc(`students/${studentF.id}`).update(EditStudent)
-           console.log(EditStudent)
-
-        Swal.fire('Guardado', student.title, 'success');
-        dispatch(startLoadingStudent(studentF.id))
-    }
-}
-
-export const startUploading = (file) => {
-    return async (dispatch) => {
-
-        Swal.fire({
-            title: 'Uploading...',
-            text: 'Please wait ...',
-            allowOutsideClick: false,
-            onBeforeOpen: () => {
-                Swal.showLoading()
-            }
-        })
-
-        fileUrl = await fileUpload(file)
-        console.log(fileUrl)
-        Swal.close()
-       return fileUrl
-    }
+export const Edit = async (student, typedoc, prog) => {
+  return async (dispatch) => {
 
     const EditStudent = {
-      image: fileUrl,
-      tittle: student.tittle,
-      description: student.description,
-      year: student.year,
-      categorie: student.categoria,
-      duration: student.duracion,
-      qualification: [],
-      trailer: "fdgfd",
-    };
+      name: student.name,
+      lastName: student.lastName,
+      fullName: student.name + ' ' + student.lastName,
+      typedoc: typedoc,
+      document: student.document,
+      email: student.email,
+      finscrip: student.finscrip,
+      prog: prog,
+    }
 
-    const studentF = { ...EditStudent };
-    delete studentF.id;
+    const studentF = { ...EditStudent }
+    delete studentF.id
 
     Swal.fire({
-      title: "actualizando...",
-      text: "Por favor, Espere ...",
+      title: 'actualizando...',
+      text: 'Por favor, Espere ...',
       allowOutsideClick: false,
       onBeforeOpen: () => {
-        Swal.showLoading();
-      },
-    });
+        Swal.showLoading()
+      }
+    })
 
-    await db.doc(`students/${studentF.id}`).update(EditStudent);
-    console.log(EditStudent);
+    await db.doc(`students/${studentF.id}`).update(EditStudent)
+    console.log(EditStudent)
 
-    Swal.fire("Guardado", student.title, "success");
-    dispatch(startLoadingStudent(studentF.id));
-  };
+    Swal.fire('Guardado', student.title, 'success');
+    dispatch(startLoadingStudent(studentF.id))
+  }
+}
+
+// export const startUploading = async (file) => {
+//   return async (dispatch) => {
+
+//     Swal.fire({
+//       title: 'Uploading...',
+//       text: 'Please wait ...',
+//       allowOutsideClick: false,
+//       onBeforeOpen: () => {
+//         Swal.showLoading()
+//       }
+//     })
+
+//     fileUrl = await fileUpload(file)
+//     console.log(fileUrl)
+//     Swal.close()
+//     return fileUrl
+//   }
+
+//   const EditStudent = {
+//     image: fileUrl,
+//     tittle: student.tittle,
+//     description: student.description,
+//     year: student.year,
+//     categorie: student.categoria,
+//     duration: student.duracion,
+//     qualification: [],
+//     trailer: "fdgfd",
+//   };
+
+//   const studentF = { ...EditStudent };
+//   delete studentF.id;
+
+//   Swal.fire({
+//     title: "actualizando...",
+//     text: "Por favor, Espere ...",
+//     allowOutsideClick: false,
+//     onBeforeOpen: () => {
+//       Swal.showLoading();
+//     },
+//   });
+
+//   await db.doc(`students/${studentF.id}`).update(EditStudent);
+//   console.log(EditStudent);
+
+//   Swal.fire("Guardado", student.title, "success");
+//   dispatch(startLoadingStudent(studentF.id));
+// };
 
 
 export const startUploading = (file) => {
---
+++ src/containers/login/LoginUser.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { getStudent } from "src/actions/studentAction";
+import emailjs from 'emailjs-com'
+
+const LoginUser = () => {
+  const [document, setDocument] = useState(null);
+  const [resultMessage, setResultMessage] = useState('')
+  const [student, setStudent] = useState({});
+
+  const handleChange = (e) => setDocument(e.target.value)
+
+  const search = async (e) => {
+    const response = await getStudent(document);
+    setStudent(response);
+  };
+
+  function sendEmail() {
+    const urlId = new Date().getTime().toString(15)
+    const certicateUrl = `${window.location.origin}/mis-certificados/${urlId}-${student.document}`
+    try {
+      const service_id = 'service_5xyb25m'
+      const template_id = 'template_vyux5d9'
+      const user_id = 'user_qC5Ej7PEzabeK2d0WuCSQ'
+      const templateParams = {
+        to_name: student.name,
+        to_email: student.email,
+        message: `Ingresa a este link para obtener tus certificados: ${certicateUrl}`,
+      };
+
+      emailjs.send(service_id, template_id, templateParams, user_id)
+        .then(function (response) {
+          if (response.status === 200) {
+            setResultMessage('Enlace enviado. Por favor revisa tu bandeja')
+          }
+        }, function (error) {
+          console.log('FAILED...', error);
+          setResultMessage('No pudimos enviar el correo. Por favor intenta más tarde')
+        });
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="px-5">
+      <h4>Para ver tus certificados ingresa tu tipo y numero de documento</h4>
+      <div className="d-flex align-items-center justify-content-center">
+        <TextField
+          label="Documento"
+          type="number"
+          variant="outlined"
+          className="mr-3"
+          onChange={handleChange}
+        />
+        <Button variant="contained" onClick={search}>
+          Buscar
+        </Button>
+      </div>
+
+      {student?.name &&
+        <div>
+          <h5>Hola, {student.name}</h5>
+          <p>Enviaremos un enlace a tu correo electronico <strong>{student.email}</strong> <br /> así podrás descargar tus certificados</p>
+          <Button variant="contained" onClick={sendEmail}>
+            Enviar certificados
+          </Button>
+        </div>
+      }
+      {student === null && <p>No se encontró</p>}
+
+      <h4 className="mt-2">{ resultMessage }</h4>
+    </div>
+  );
+};
+
+export default LoginUser;
