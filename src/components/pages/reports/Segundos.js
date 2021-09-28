6d9c2a608f9d734f551f1a3590c50bc99ee8fb24
Gerald <gejocadev@gmail.com>
2021-09-28 03:35:24 -0500

    .

--- src/components/pages/reports/Segundos.js
+++ src/components/pages/reports/Segundos.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import MaterialTable from 'material-table'
-import {Button} from '@material-ui/core';
+import MaterialTable from 'material-table';
 /*{import {makeStyles} from '@material-ui/core/styles';}*/
 import StudentDetailSem2 from '@components/pages/modals/StudentDetailSem2';
 import StudentAddModal from '@components/pages/modals/AddStudent';
@@ -40,9 +39,6 @@ import { activeStudents } from '@actions/studentAction';
 
         dispatch(activeStudents(data.id, data))
         setShowDetail(true)
-    }
-    const handleShowAdd = () => {
-        setShowAddStudent(true)
     }
 
     const columnas = [
@@ -114,7 +110,6 @@ import { activeStudents } from '@actions/studentAction';
                 }}
            />
            <div>{StudentDetailSem2(showDetail, setShowDetail)}</div>
-           <Button onClick={() => handleShowAdd()}>Añadir Estudiante</Button>
            <div>{StudentAddModal(showAddStudent, setShowAddStudent)}</div>
         </div>
     )
